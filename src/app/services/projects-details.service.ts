9c4b926fc835af7ac0d338d927b1c227ba1f269c
Banner Samuelson <bannersamuelson@gmail.com>
2023-07-15 16:19:23 -0400

    about section worked on

--- src/app/services/projects-details.service.ts
+++ src/app/services/projects-details.service.ts
@@ -12,9 +12,8 @@ export class ProjectDetailsService {
   foodDetails = [
     {
       id: 1,
-      foodName: "Paneer Grilled Sandwich",
-      foodDetails: "Pan-fried masala paneer.",
-      foodPrice: 200,
+      foodName: "Amazon Prime",
+      foodDetails: "Amazon Prime",
       foodImg: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wzfq7djolqxgdhghebbq",
       foodImg2: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wzfq7djolqxgdhghebbq",
       foodImg3: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wzfq7djolqxgdhghebbq"
@@ -22,37 +21,32 @@ export class ProjectDetailsService {
     },
     {
       id: 2,
-      foodName: "Veggie Supreme",
+      foodName: "Bahel Coffee Roaster",
       foodDetails: "Onion| Green Capsicum|Mushroom |black olives , sweet corn , Red Paprika topped with Cheese",
-      foodPrice: 369,
       foodImg: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/sgbobtbxlojbtdnr2m5k"
     },
     {
       id: 3,
-      foodName: "Paneer Burger",
+      foodName: "Modern Rugs",
       foodDetails: "panner",
-      foodPrice: 149,
       foodImg: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/xbeqlsck3p0kei53to7k"
     },
     {
       id: 4,
-      foodName: "Veg Masala Roll In Naan",
+      foodName: "HerFit App",
       foodDetails: "A homely mix of mashed potato and veggies, seasoned with Indian spices. A filling sure to take you back to mom's kitchen.",
-      foodPrice: 140,
       foodImg: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/l2ng6gtge30sqaafqng7"
     },
     {
       id: 5,
-      foodName: "Indulgence Brownie",
+      foodName: "Geneia",
       foodDetails: "(Eggless) Indulge in richly decadent chocolate brownie crafted with love & topped with bitter-sweet chocolate that provides ultra-rich chocolate experience.",
-      foodPrice: 105,
       foodImg: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/iqlmbg1hlyc0dspdyzzv"
     },
     {
       id: 6,
-      foodName: "Oreo Cheesecake Ice Cream",
+      foodName: "Christopher Fareed",
       foodDetails: "Oreo ice cream",
-      foodPrice: 219,
       foodImg: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wtj8esaeslvlscv8glj6"
     }
   ]
